c88ad1e21a8e6fe8baead7327cccc977df34f781
covector <kinchancheuk@gmail.com>
2024-05-22 00:20:14 +0800

    scripts for updating pack without git

---
+++ update-cmc-mobarena-texture.bat
@@ -0,0 +1,7 @@
+@echo off
+
+curl https://github.com/covector/CMC-MobArena-Textures/archive/refs/heads/master.zip -L --ssl-no-revoke -o cmc-mobarena-texture-temp.zip
+tar -xf cmc-mobarena-texture-temp.zip
+xcopy .\CMC-MobArena-Textures-master\* .\ /E /H /C /Y
+rmdir /S /Q CMC-MobArena-Textures-master
+del cmc-mobarena-texture-temp.zip
---
+++ update-cmc-mobarena-texture.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://github.com/covector/CMC-MobArena-Textures/archive/refs/heads/master.zip -L --ssl-no-revoke -o cmc-mobarena-texture-temp.zip
+# REQUIRES UNZIP
+unzip -o cmc-mobarena-texture-temp.zip
+cp -r CMC-MobArena-Textures-master/* .
+rm -r ./CMC-MobArena-Textures-master
+rm cmc-mobarena-texture-temp.zip
